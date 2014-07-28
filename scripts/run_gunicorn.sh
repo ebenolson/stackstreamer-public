cdb245b2d64d456e299cb5f168d6a403b5252194
Eben Olson <eben.olson@gmail.com>
2014-07-28 13:16:45 -0400

    add gunicorn integration

---
+++ scripts/run_gunicorn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e 
+LOGFILE=/web/logs/gunicorn.log 
+LOGDIR=$(dirname $LOGFILE)
+NUM_WORKERS=2 
+USER=django 
+GROUP=web ADDRESS=127.0.0.1:8000
+
+cd /web/django/stackstreamer
+source venv/bin/activate
+test -d $LOGDIR || mkdir -p $LOGDIR
+exec gunicorn -w $NUM_WORKERS --bind=$ADDRESS \
+  --user=$USER --group=$GROUP --log-level=debug \
+  --log-file=$LOGFILE stackstreamer.wsgi:application
