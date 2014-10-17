117376bcc4d739eee022392ce6456d39bbdaa3ce
Eben Olson <eben.olson@gmail.com>
2014-10-17 15:28:55 -0400

    setup script

--- setup-scripts/setup.sh
+++ setup-scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp /web/setup-scripts/celeryd.conf.example /etc/init/celeryd.conf
-ln -s /lib/init/upstart-job /etc/init.d/celeryd
+cp /web/setup-scripts/celeryd.conf.example /etc/default/celeryd
+ln -s /web/setup-scripts/celeryd.sh.example /etc/init.d/celeryd
 
 cp /web/setup-scripts/gunicorn.conf.example /etc/init/gunicorn.conf
 ln -s /lib/init/upstart-job /etc/init.d/gunicorn
